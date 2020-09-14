454ae4c82d4984c9c83a15cae6c2bdb32c6e0e74
shahab valizade <shahabvalizade@gmail.com>
2020-09-14 15:04:24 +0430

    bootstrap and jquery added.

--- functions.php
+++ functions.php
@@ -140,12 +140,16 @@ add_action( 'widgets_init', 'shahabtheme_widgets_init' );
  * Enqueue scripts and styles.
  */
 function shahabtheme_scripts() {
-	wp_enqueue_style( 'shahabtheme-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'shahabtheme-style', 'rtl', 'replace' );
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), _S_VERSION );
+    wp_enqueue_style( 'shahabtheme-style', get_stylesheet_uri(), array(), _S_VERSION );
+
+    wp_style_add_data( 'shahabtheme-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'shahabtheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery.js', array(), _S_VERSION, true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
