f4173ff9f913493a7dc05f881f339878d29e7f5c
shahab valizade <shahabvalizade@gmail.com>
2020-09-20 10:57:31 +0430

    header logo style added.

--- header.php
+++ header.php
@@ -31,18 +31,20 @@
 			the_custom_logo();
 			if ( is_front_page() || is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$shahabtheme_description = get_bloginfo( 'description', 'display' );
-			if ( $shahabtheme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $shahabtheme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+                <div class="site-title-description">
+				        <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				        <?php
+			        else :
+				        ?>
+				        <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				        <?php
+			        endif;
+			        $shahabtheme_description = get_bloginfo( 'description', 'display' );
+			        if ( $shahabtheme_description || is_customize_preview() ) :
+				        ?>
+				        <p class="site-description"><?php echo $shahabtheme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			        <?php endif; ?>
+                </div>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
