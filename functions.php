ab7c43413f0d03864ff3ec2826fee868334226be
shahab valizade <shahabvalizade@gmail.com>
2020-09-26 09:15:21 +0430

    navigation.js file added.

--- functions.php
+++ functions.php
@@ -147,6 +147,8 @@ function shahabtheme_scripts() {
 
     wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array(), _S_VERSION, true );
     wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'shahabtheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
