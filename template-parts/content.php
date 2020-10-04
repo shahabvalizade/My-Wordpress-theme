47aaf68572fa63e43931cd2b056bc071540a1d86
shahab valizade <shahabvalizade@gmail.com>
2020-10-04 12:05:12 +0430

    post cover div added.

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,26 +10,28 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+	<div id="post-cover">
+        <header class="entry-header">
+	    	<?php
+	    	if ( is_singular() ) :
+	    		the_title( '<h1 class="entry-title">', '</h1>' );
+	    	else :
+	    		the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+	    	endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				shahabtheme_posted_on();
-				shahabtheme_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+	    	if ( 'post' === get_post_type() ) :
+	    		?>
+	    		<div class="entry-meta">
+	    			<?php
+	    			shahabtheme_posted_on();
+	    			shahabtheme_posted_by();
+	    			?>
+	    		</div><!-- .entry-meta -->
+	    	<?php endif; ?>
+	    </header><!-- .entry-header -->
 
-	<?php shahabtheme_post_thumbnail(); ?>
+	    <?php shahabtheme_post_thumbnail(); ?>
+    </div>
 
 	<div class="entry-content">
 		<?php
