10b48c9ae77f44e2fac5108f0f50aabe8d4586ed
shahab valizade <shahabvalizade@gmail.com>
2020-10-31 10:03:37 +0430

    atchive page modified.

--- archive.php
+++ archive.php
@@ -10,18 +10,21 @@
 get_header();
 ?>
 
+    <?php if ( have_posts() ) : ?>
+
+    <header class="page-header">
+        <?php
+        the_archive_title( '<h1 class="page-title">', '</h1>' );
+        the_archive_description( '<div class="archive-description">', '</div>' );
+        ?>
+    </header><!-- .page-header -->
+
+    <?php endif; ?>
+
 	<main id="primary" class="site-main">
         <section class="post-articles">
 
         <?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) :
@@ -36,7 +39,7 @@ get_header();
 
 			endwhile;
 
-			the_posts_navigation();
+			the_posts_pagination();
 
 		else :
 
