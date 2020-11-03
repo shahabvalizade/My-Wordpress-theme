3de472001e42e61f62ba5d3563f809a2d6c376e7
shahab valizade <shahabvalizade@gmail.com>
2020-11-03 15:15:05 +0430

    404 page restructured.

--- 404.php
+++ 404.php
@@ -10,49 +10,21 @@
 get_header();
 ?>
 
+    <header class="page-header">
+        <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'shahabtheme' ); ?></h1>
+    </header><!-- .page-header -->
+
 	<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'shahabtheme' ); ?></h1>
-			</header><!-- .page-header -->
-
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'shahabtheme' ); ?></p>
+				<p class="text-404"><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'shahabtheme' ); ?></p>
 
 					<?php
 					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'shahabtheme' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$shahabtheme_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'shahabtheme' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$shahabtheme_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
 
 			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+            <?php get_sidebar('no-content'); ?>
 
 	</main><!-- #main -->
 
