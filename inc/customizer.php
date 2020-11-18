b333b840cda372940cc2803b28ee938ae06022d3
shahab valizade <shahabvalizade@gmail.com>
2020-11-18 10:40:55 +0430

    add customizer setting,control,section.

--- inc/customizer.php
+++ inc/customizer.php
@@ -31,6 +31,63 @@ function shahabtheme_customize_register( $wp_customize ) {
 			)
 		);
 	}
+
+	//add new section for theme settings
+    $wp_customize->add_section( 'theme_options',
+        array(
+            'title'			=> __( 'Theme Options', 'shahabtheme' ),
+            'priority'		=> 95,
+            'capability'	=> 'edit_theme_options',
+            'description'	=> __( 'Change Theme Settings', 'shahabtheme' )
+        )
+    );
+
+    // Create excerpt or full content settings
+    $wp_customize->add_setting(	'length_setting',
+        array(
+            'default'			=> 'excerpt',
+            'type'				=> 'theme_mod',
+            'sanitize_callback' => 'shahabtheme_sanitize_length', // Sanitization function appears further down
+            'transport'			=> 'postMessage'
+        )
+    );
+
+    // Add the controls
+    $wp_customize->add_control(	'shahabtheme_length_control',
+        array(
+            'type'		=> 'radio',
+            'label'		=> __( 'Index/archive displays', 'shahabtheme' ),
+            'section'	=> 'theme_options',
+            'choices'	=> array(
+                'excerpt'		=> __( 'Excerpt (default)', 'shahabtheme' ),
+                'full-content'	=> __( 'Full content', 'shahabtheme' )
+            ),
+            'settings'	=> 'length_setting' // Matches setting ID from above
+        )
+    );
+
+    // Setting for header and footer background color
+    $wp_customize->add_setting( 'theme_background_color', array(
+        'default' => '#072237',
+        'transport' => 'postMessage',
+        'type' => 'theme_mod',
+        'sanitize_callback' => 'sanitize_hex_color',
+    ));
+
+    // Control for header and footer background color.
+    $wp_customize->add_control(
+        new WP_Customize_Color_Control(
+            $wp_customize,
+            'theme_background_color', array(
+                'label' => __( 'Header and footer background color', 'shahabtheme'),
+                'section' => 'colors',
+                'settings' => 'theme_background_color'
+            )
+        )
+    );
+
+
+
 }
 add_action( 'customize_register', 'shahabtheme_customize_register' );
 
@@ -41,6 +98,13 @@ add_action( 'customize_register', 'shahabtheme_customize_register' );
  */
 function shahabtheme_customize_partial_blogname() {
 	bloginfo( 'name' );
+}
+
+function shahabtheme_sanitize_length( $value ) {
+    if ( ! in_array( $value, array( 'excerpt', 'full-content' ) ) ) {
+        $value = 'excerpt';
+    }
+    return $value;
 }
 
 /**
@@ -59,3 +123,61 @@ function shahabtheme_customize_preview_js() {
 	wp_enqueue_script( 'shahabtheme-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
 }
 add_action( 'customize_preview_init', 'shahabtheme_customize_preview_js' );
+
+
+
+
+if ( ! function_exists( 'shahabtheme_header_style' ) ) :
+    /**
+     * Styles the header image and text displayed on the blog.
+     *
+     * @see shahabtheme_custom_header_setup().
+     */
+    function shahabtheme_header_style() {
+        $header_text_color = get_header_textcolor();
+        $theme_bg_color = get_theme_mod('theme_background_color');
+
+        /*
+         * If no custom options for text are set, let's bail.
+         * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
+         */
+        if ( get_theme_support( 'custom-header', 'default-text-color' ) != $header_text_color ) {
+
+            // If we get this far, we have custom styles. Let's do this.
+            ?>
+            <style type="text/css">
+                <?php
+                // Has the text been hidden?
+                if ( ! display_header_text() ) :
+                    ?>
+                .site-title,
+                .site-description {
+                    position: absolute;
+                    clip: rect(1px, 1px, 1px, 1px);
+                }
+                <?php
+                // If the user has set a custom color for the text use that.
+            else :
+                ?>
+                .site-title a,
+                .site-description {
+                    color: #<?php echo esc_attr( $header_text_color ); ?>;
+                }
+                <?php endif; ?>
+            </style>
+        <?php
+        }
+
+        if ( '#072237' != $theme_bg_color ) {
+            ?>
+            <style type="text/css">
+                #footer,
+                .site-branding,
+                article #post-cover {
+                    background-color: <?php echo esc_attr($theme_bg_color); ?>;
+                }
+            </style>
+            <?php
+        }
+    }
+    endif;
